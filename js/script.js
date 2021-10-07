ecfaad5712fb90834472d72ff5ff6a7a7d01873a
Kimbangul <highcolor_12@naver.com>
2021-10-07 19:19:12 +0900

    모바일 메뉴 밖을 터치하면 닫히도록 수정

--- js/script.js
+++ js/script.js
@@ -22,27 +22,44 @@ $(document).ready(function() {
 
     }
 
+    
+
     function mbNav_init(){
-        $mbNav = $('#main-header > .sns-btn');
+        const $mbNav = $('#main-header > .sns-btn');
+        const $mbNavList = $mbNav.find('li > a');
         
         $mbNav.click(function(e){
 
             if ($mbNav.hasClass('active')){
-                if (e.target === $('div.sns-btn')){
+                if (e.target !== e.currentTarget){
                     // 리스트를 클릭했을 때 리스트가 사라지지 않도록 이벤트 전파를 막아줌.
-                    console.log(e.target);
-                    return;
+                    return;                    
                 }
                 else{
                     $mbNav.removeClass('active');
                     // 리스트가 아닌 메뉴 버튼을 클릭했을 때
                 }
-            } else {
+            } else {    
                 $mbNav.addClass('active');
             }
         });
 
-        
+        $("html").on('click', function(e){
+
+            if (e.target !== $mbNav[0] && $mbNav.hasClass('active')){
+                for (let i = 0; i <= $mbNavList.length - 1; i++){
+                    console.log(e.target, $mbNavList[i]);
+                    if (e.target === $mbNavList[i]){
+                        return;
+                    }                
+                }               
+                $mbNav.removeClass('active');
+               }
+
+            
+
+           
+        });
     }
 
     fullpage_init();
