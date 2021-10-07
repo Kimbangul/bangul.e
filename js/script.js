b4917a7e97ca7ac0f6f2b7ead5e0ec3c0e31ed7b
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2021-10-07 16:13:02 +0900

    모바일 menu 버튼 구현

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 
-
 $(document).ready(function() {
-
-
 
     function fullpage_init(){
 
@@ -25,7 +22,30 @@ $(document).ready(function() {
 
     }
 
-    fullpage_init();
+    function mbNav_init(){
+        $mbNav = $('#main-header > .sns-btn');
+        
+        $mbNav.click(function(e){
 
+            if ($mbNav.hasClass('active')){
+                if (e.target === $('div.sns-btn')){
+                    // 리스트를 클릭했을 때 리스트가 사라지지 않도록 이벤트 전파를 막아줌.
+                    console.log(e.target);
+                    return;
+                }
+                else{
+                    $mbNav.removeClass('active');
+                    // 리스트가 아닌 메뉴 버튼을 클릭했을 때
+                }
+            } else {
+                $mbNav.addClass('active');
+            }
+        });
+
+        
+    }
+
+    fullpage_init();
+    mbNav_init();
 
 });
