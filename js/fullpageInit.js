710728dc21152f3dabe4cab77b653b2039ada719
Kimbangul <highcolor_12@naver.com>
2021-10-10 02:43:44 +0900

    애니메이션 실행 문제 픽스
    enquire.js를 이용해 모바일 화면에서 애니메이션이 실행되지 않는 문제 픽스

--- js/fullpageInit.js
+++ js/fullpageInit.js
@@ -2,9 +2,6 @@ $(document).ready(function () {
 
     function fullpage_init() {
 
-
-
-
         $('#contents').fullpage({
             //options here
             licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
@@ -15,37 +12,58 @@ $(document).ready(function () {
             showActiveTooltip: true,
             scrollingSpeed: 1000,
             scrollOverflow: true,
-            responsiveWidth: 1000,
+            responsiveWidth: 1000, //1000px 이하에서 fullpage 해제
 
         });
 
     }
 
     function scrollMagic_init() {
-        var controller = new ScrollMagic.Controller();
-
-        function animation1() {
-
-            var revealElements = $('section');
-
+        
+        function makeScrollMagic() {
+            var controller = new ScrollMagic.Controller();
+            var revealElements = $('section'); // 타겟 등록
 
             for (var i = 0; i < revealElements.length; i++) {
-                var scene1 = new ScrollMagic.Scene({
+
+                var sectionAni = new ScrollMagic.Scene({
                         triggerElement: revealElements[i],
                         triggerHook: 0.5, //viewport에 대해 상대적으로 어느 시점에서 보여줄 건지를 설정
-                        offset: -300,
-                        // duration: "100%",
+                        offset: 0,
                     })
-
+                    .addIndicators({
+                        name:"section" + i
+                      })
                     .setClassToggle(revealElements[i], "active")
                     .addTo(controller); // 컨트롤러 등록                
-
             }
-        }       
-      
-        animation1();
-    }
+        }
 
+        function destoryScrollMagic(){
+            controller.destroy(true);
+            controller = null;
+            sectionAni.destory(true);
+            scene = null;
+        }
+
+        // enquire.js
+
+        enquire.register("all and (max-width: 999px)", {
+            match : function() {
+                console.log('scrollMagic init');
+                makeScrollMagic();
+            },
+            unmatch : function() {
+                console.log('scrollMagic destory');
+                destoryScrollMagic();
+            }
+    });
+
+
+
+
+    }
+    scrollMagic_init();
     fullpage_init();
 
 
