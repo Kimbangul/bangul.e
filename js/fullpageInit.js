afc07ea47edcd1a39382906102d4b73b8783a2fb
Kimbangul <highcolor_12@naver.com>
2021-10-09 15:09:49 +0900

    오리온 상세페이지 작업

--- js/fullpageInit.js
+++ js/fullpageInit.js
@@ -21,6 +21,31 @@ $(document).ready(function () {
 
     }
 
+    function scrollMagic_init() {
+        var controller = new ScrollMagic.Controller();
+
+        function animation1() {
+
+            var revealElements = $('section');
+
+
+            for (var i = 0; i < revealElements.length; i++) {
+                var scene1 = new ScrollMagic.Scene({
+                        triggerElement: revealElements[i],
+                        triggerHook: 0.5, //viewport에 대해 상대적으로 어느 시점에서 보여줄 건지를 설정
+                        offset: -300,
+                        // duration: "100%",
+                    })
+
+                    .setClassToggle(revealElements[i], "active")
+                    .addTo(controller); // 컨트롤러 등록                
+
+            }
+        }       
+      
+        animation1();
+    }
+
     fullpage_init();
 
 
